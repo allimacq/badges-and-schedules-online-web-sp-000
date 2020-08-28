4d5d36339a28db04062e57a3b51cd42d54f33832
Allison MacQueen <allimacq@gmail.com>
2020-08-28 19:52:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,19 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
+  #initializing new array this method will return
   greeting = Array.new()
+  #initializing index
   i = 0
+  #for each name in the original array...
   names.each do |x|
+    #have this greeting and put each one in the new array
     greeting[i] = "Hello, my name is #{x}."
+    #changing index
     i += 1
   end
+  #returning finished array
   return greeting
 end
+
+def assign_rooms()

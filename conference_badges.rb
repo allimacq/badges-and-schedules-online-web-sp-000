5838ee84d2dac75b9b848eccec37a654a3746f36
Allison MacQueen <allimacq@gmail.com>
2020-08-28 20:18:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,5 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(name)
-  name.each_with_index {|room, index| p "Hello, #{name}! You'll be assigned to room #{room}!"  }
+  name.each_with_index {|name, index| p "Hello, #{name}! You'll be assigned to room #{room}!"  }
 end

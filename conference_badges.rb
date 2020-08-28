783b18b2b6e0fec6f8ea8eeb19c2ae07c9fa78b0
Allison MacQueen <allimacq@gmail.com>
2020-08-28 19:50:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(names)
   greeting = Array.new()
   i = 0
   names.each do |x|
-    greeting[i] = "Hello, my name is #{x}"
+    greeting[i] = "Hello, my name is #{x}."
     i += 1
   end
   return greeting
